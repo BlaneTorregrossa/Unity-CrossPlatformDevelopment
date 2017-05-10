dae6a8f17ad61f0e8525f74c66b227b2dbc0189c
BlaneTorregrossa <blanet97@cox.net>
2017-05-10 13:51:54 -0500

    not much

--- Assets/Scripts/BulletPattern.cs
+++ Assets/Scripts/BulletPattern.cs
@@ -4,47 +4,106 @@ using UnityEngine;
 
 public class BulletPattern : MonoBehaviour
 {
+    public Vector2 Graph;
+    public List<Vector2> Bullets = new List<Vector2>();
+    public Vector2 PlayerVec;
+    public GameObject Bullet;
+    public Rigidbody rb;
 
-    public GameObject BulletSpawner;
+    private Vector3 NewBulletPos = new Vector3(12, 0.0001f, -7.0f);
+    private float positionScaler = 2.25f;
+    private int xCounter = 0;
+    private int BulletRemovalChoice;
 
-    private int Pattern = 0;
+    // Creates Graph 
+    void GenGraph()
+    {
+        if (BulletRemovalChoice == 0)
+        {
+            CreateFirstPattern();
+        }
 
-    void GnerateBullets()
+        //if (Graph.x >= 5 || Graph.y >= 5)
+        //{
+        //    xCounter = 0;
+        //}
+
+        //else
+        //{
+        //    while (Graph.x != xCounter)
+        //    {
+        //        Bullets.Add(Graph);
+        //    }
+        //    while (Graph.x == xCounter)
+        //    {
+        //        Bullets.Add(Graph);
+        //        Graph.y++;
+        //        NewBulletPos += new Vector3(0, 0, 1) * positionScaler;
+        //        Instantiate(Bullet).transform.position = NewBulletPos;
+        //        if (Graph.y >= 5)
+        //        {
+
+        //            NewBulletPos.x += 1 * positionScaler;
+        //            NewBulletPos.z = -7.0f;
+        //            Graph.y = 0;
+        //            Graph.x++;
+        //            xCounter++;
+        //            GenGraph();
+        //        }
+        //    }
+        //}
+    }
+
+    void ChoosePattern()
+    {
+        while (BulletRemovalChoice == 0)
+        {
+            BulletRemovalChoice = Random.Range(1, 2);
+        }
+        GenGraph();
+    }
+
+    void CreateFirstPattern()
+    {
+        if (Graph.x >= 5 || Graph.y >= 5)
+        {
+            xCounter = 0;
+        }
+
+        else
+        {
+            while (Graph.x != xCounter)
+            {
+                Bullets.Add(Graph);
+            }
+            while (Graph.x == xCounter)
+            {
+                Bullets.Add(Graph);
+                Graph.y++;
+                NewBulletPos += new Vector3(0, 0, 1) * positionScaler;
+                Instantiate(Bullet).transform.position = NewBulletPos;
+                if (Graph.y >= 5)
+                {
+
+                    NewBulletPos.x += 1 * positionScaler;
+                    NewBulletPos.z = -7.0f;
+                    Graph.y = 0;
+                    Graph.x++;
+                    xCounter++;
+                    GenGraph();
+                }
+            }
+        }
+    }
+
+    void Start()
     {
 
     }
 
-    void Patterns()
+    void Update()
     {
-        if (Pattern == 1)
-        {
-            
-        }
-        if (Pattern == 2)
-        {
-
-        }
-        if (Pattern == 3)
-        {
-
-        }
-        if (Pattern == 4)
-        {
-
-        }
-        if (Pattern == 5)
-        {
-
-        }
+        ChoosePattern();
     }
 
-	void Start ()
-    {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
 }
